f772b7fafb33f0617939123114864388fde9eabc
mothpeople <mothpeople.vn@gmail.com>
2025-12-01 22:22:53 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
-// BUMPED TO v17 TO FORCE UPDATE AND FIX LOADING ISSUES
-const CACHE_NAME = 'stay-dry-hcmc-v17';
+// BUMPED TO v106 TO MATCH HTML AND FORCE UPDATE
+const CACHE_NAME = 'stay-dry-hcmc-v106';
 
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/logo.png', // Make sure to cache your logo!
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
 self.addEventListener('install', event => {
+  // Force this new worker to become active immediately, skipping the wait
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -25,6 +28,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
+          // Delete old caches that don't match the current version
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
@@ -33,6 +37,7 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  // Tell the active service worker to take control of the page immediately
   self.clients.claim();
 });
 
@@ -40,6 +45,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Return cached file if found, otherwise fetch from network
         if (response) {
           return response;
         }
