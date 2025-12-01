67e68cfb4b151b68f08ef9d46d0e4076789234d8
mothpeople <mothpeople.vn@gmail.com>
2025-12-01 21:31:50 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
-// BUMPED TO v11 TO FORCE UPDATE ON PHONE
-const CACHE_NAME = 'stay-dry-hcmc-v11';
+// BUMPED TO v14 TO FORCE UPDATE AND FIX PREVIEWS
+const CACHE_NAME = 'stay-dry-hcmc-v14';
 
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  // We can optionally cache the script, but usually the index.html cache is sufficient
+  // as long as the version query param (?v=...) changes in the HTML.
 ];
 
 self.addEventListener('install', event => {
   // Force this new worker to become active immediately, skipping the wait
-  self.skipWaiting(); 
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -26,9 +28,10 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          // Delete old caches (v1 through v10)
+          // Delete old caches that don't match the current version
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName); 
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
           }
         })
       );
