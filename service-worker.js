f1bd631cb7196345505e867df7f14a6c0454b2fb
mothpeople <mothpeople.vn@gmail.com>
2025-12-01 21:45:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,15 @@
-// BUMPED TO v14 TO FORCE UPDATE AND FIX PREVIEWS
-const CACHE_NAME = 'stay-dry-hcmc-v14';
+// BUMPED TO v16 TO FORCE UPDATE
+const CACHE_NAME = 'stay-dry-hcmc-v16';
 
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-  // We can optionally cache the script, but usually the index.html cache is sufficient
-  // as long as the version query param (?v=...) changes in the HTML.
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
 self.addEventListener('install', event => {
-  // Force this new worker to become active immediately, skipping the wait
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -28,7 +25,6 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          // Delete old caches that don't match the current version
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
@@ -37,7 +33,6 @@ self.addEventListener('activate', event => {
       );
     })
   );
-  // Tell the active service worker to take control of the page immediately
   self.clients.claim();
 });
 
@@ -45,7 +40,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Return cached file if found, otherwise fetch from network
         if (response) {
           return response;
         }
